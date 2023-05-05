bf1a6598125a21769712d2ba0998aa92e77d08fd
Pavel Zhuravlyov <zd.kane@hotmail.com>
2023-05-05 23:26:21 +0300

    Initial servlet commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AccelPunch_server
 AccelPunch: REST server for data aggregation and statistics
+
+Server: NodeJS
+Database: Postgres
+
+1. Create database with name "AccelPunch_database"
+2. Run `AccelPunch_schema.sql` for schema import
+3. Run `npm install`
+4. Create copy of `.env.example` file into new `.env` file and fill values  accordingly
+4. Run `node servlet.js`
---
+++ servlet.js
@@ -0,0 +1,91 @@
+const http = require('http');
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+class Connection {
+  constructor() {
+    this.pool = new Pool({
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      database: process.env.PGDATABASE,
+      password: process.env.PGPASSWORD,
+      port: process.env.PGPORT,
+    });
+  }
+  
+  query = async (query) => {
+    try {
+      await this.pool.connect()
+      const res = await this.pool.query(query)
+      console.log(res)
+      // await this.pool.end()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+};
+
+let conn = new Connection();
+
+const server = http.createServer((req, res) => {
+  let op = "GET";
+  if( req.method === 'GET' ) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello, GET!\n');
+  } else if( req.method === 'POST' ) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello, POST!\n');
+    op = "POST";
+    const chunks = [];
+    req.on('data', chunk => chunks.push(chunk));
+    req.on('end', () => {
+      const json_str = Buffer.concat(chunks);
+      const json = JSON.parse(json_str);
+      // console.log(JSON.stringify(json, null, 2));
+      // console.log(json.gloves.length);
+      stmt = "INSERT INTO ";
+      let recordCount = 0;
+      if (json.hasOwnProperty("gloves")) {
+        stmt += "gloves (timestamp, glove, x, y, z) VALUES "
+        json.gloves.forEach(glove => {
+          if (recordCount > 0) {
+            stmt += ","
+          }
+          stmt += "(" +
+                    glove.timestamp + "," +
+                    "'"+glove.glove+"'" + "," +
+                    glove.x + "," +
+                    glove.y + "," +
+                    glove.z + ")";
+          recordCount++;
+        });
+      } else if (json.hasOwnProperty("bag")) {
+        stmt += "bag (timestamp, x, y, z, temperature) VALUES "
+        json.bag.forEach(bagRecord => {
+          if (recordCount > 0) {
+            stmt += ","
+          }
+          stmt += "(" +
+                    bagRecord.timestamp + "," +
+                    bagRecord.x + "," +
+                    bagRecord.y + "," +
+                    bagRecord.z + "," +
+                    bagRecord.temperature + ")";
+          recordCount++;
+        });
+      }
+      // console.log(stmt);
+      stmt += " ON CONFLICT DO NOTHING";
+      conn.query(stmt);
+    })
+  }
+  console.log(`${op} from ${req.socket.remoteAddress}`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
